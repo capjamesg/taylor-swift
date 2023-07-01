4bc8f5a2b21273163538d184337c6b703cfb1d21
capjamesg <jamesg@jamesg.blog>
2023-07-02 00:37:19 +0100

    update code

--- web.py
+++ web.py
@@ -1,11 +1,14 @@
 import data
 import torch
-# from models import imagebind_model
-# from models.imagebind_model import ModalityType
+from models import imagebind_model
+from models.imagebind_model import ModalityType
 import os
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify
+import tempfile
+import librosa
+import numpy as np
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./static")
 
 def get_embedding(audio):
     audio = {
@@ -15,21 +18,21 @@ def get_embedding(audio):
 
     return audio_embedding[ModalityType.AUDIO]
 
-# device = "cuda:0" if torch.cuda.is_available() else "cpu"
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-# # Instantiate model
-# model = imagebind_model.imagebind_huge(pretrained=True)
-# model.eval()
-# model.to(device)
+# Instantiate model
+model = imagebind_model.imagebind_huge(pretrained=True)
+model.eval()
+model.to(device)
 
 vectors = []
 
 with torch.no_grad():
-    for file in os.listdir("../"):
+    for file in os.listdir("./data/"):
         # if ends in wav
         if file.endswith(".wav") and "_" not in file:
             a = {
-                ModalityType.AUDIO: data.load_and_transform_audio_data(["../" + file], device),
+                ModalityType.AUDIO: data.load_and_transform_audio_data(["./data/" + file], device),
             }
             a_embedding = model(a)
 
@@ -38,32 +41,32 @@ with torch.no_grad():
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        print(request.data)
-        # get body from request audio/wav, as blb
-        print(request.files)
-        file = request.files['file']
-
-        
-
-        import librosa
-
-        # plot spectrogram
-        import matplotlib.pyplot as plt
-        import librosa.display
-        import numpy as np
-
-        y, sr = librosa.load(file)
-
-        # if length is > 10 seconds, return error
-        # plot as spectrogram
-        
-        # if audio.length > 10:
-        #     return render_template("index.html", error="Audio must be less than 10 seconds")
-
+        # get body from request audio/ogg; codecs=opus
+        audio = request.files["file"]
         # get similarity
         with torch.no_grad():
+                # uses torchvision to load audio, transform as necessary please
+            # audio = n
+            # create tmp file
+
+            # create tmp file
+            tmp = tempfile.NamedTemporaryFile()
+            audio.save(tmp.name)
+
+            # clean up audio with librosa
+            # y, sr = librosa.load(tmp.name)
+
+            # # remove silence
+            # y, _ = librosa.effects.trim(y)
+
+            # # convert to mono
+            # y = librosa.to_mono(y)
+
+            # # save as wav
+            # librosa.output.write_wav(tmp.name, y, sr)
+            
             audio = {
-                ModalityType.AUDIO: data.load_and_transform_audio_data([audio], device),
+                ModalityType.AUDIO: data.load_and_transform_audio_data([tmp.name], device),
             }
 
             audio_embedding = model(audio)
@@ -72,15 +75,26 @@ def index():
 
             for v in vectors:
                 sim = torch.cosine_similarity(audio_embedding[ModalityType.AUDIO], v)
+                print(sim)
                 sims.append(sim)
 
             # print avg. sim
-            print(sum(sims) / len(sims))
+            avg_sim = sum(sims) / len(sims)
+
+            avg_sim = avg_sim.cpu().numpy()
+
+            # cast from float32 to float py
+            avg_sim = avg_sim.astype(float)
+            
+            # round to 2 decimal places
+            avg_sim = np.round(avg_sim, 2)
+
+            print(avg_sim)
 
         # return similarity
-        return render_template("index.html", sim=sim)
+        return jsonify({"similarity": avg_sim[0]})
     
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8084)
